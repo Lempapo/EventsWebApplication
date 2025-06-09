943534b138d75bf63adcb92e07f66af7597e0dc7
Vlad <vlad.lemtyugov@mail.ru>
2025-06-09 10:36:56 +0300

    Add Event entity.

---
+++ EventsWebApplication/Entities/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace EventsWebApplication.Entities;
+
+public class Event
+{
+    public Guid Id { get; init; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartAt { get; set; }
+    public DateTime EndAt { get; set; }
+    public string Location { get; set; }
+    public string Category { get; set; }
+    public int MaxParticipantsCount { get; set; }
+    public string ImageUrl { get; set; }
+}
